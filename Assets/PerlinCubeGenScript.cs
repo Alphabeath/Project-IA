f3311c17f0f0f60f3edd1133fa00bb2aff0fc927
Alphabeath <bryanreyesavila@outlook.cl>
2022-06-05 13:00:57 -0400

    Inicio de código

---
+++ Assets/PerlinCubeGenScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinCubeGenScript : MonoBehaviour
+{
+    public float perlinNoise = 0f;
+    public float refiment = 0f;
+    public float multiplier = 0f;
+    public int cubes = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < cubes; i++)
+        {
+            for(int j = 0; j < cubes; j++)
+            {
+                perlinNoise = Mathf.PerlinNoise(i * refiment, j * refiment);
+                GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                go.transform.position = new Vector3(i, perlinNoise * multiplier, j);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/PerlinMoveScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinMoveScript : MonoBehaviour
+{
+    public float elapseTime = 0f;
+    public float perlinNoise = 0f;
+    public float multiplier = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapseTime = Time.time;
+        perlinNoise = Mathf.PerlinNoise(elapseTime, 0);
+        transform.position = new Vector3(transform.position.x, perlinNoise * multiplier, transform.position.z);  
+    }
+}
