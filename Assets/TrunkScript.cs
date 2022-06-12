fa8a79b6b8a3f7d1e4e8c46e4545d776868de985
mellokx <hernanvicentecaro@gmail.com>
2022-06-12 19:11:18 -0400

    tronco que se pone en posicion negativa

---
+++ Assets/TrunkScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrunkScript : MonoBehaviour
+{
+    public GameObject prefab;
+    public Vector3 size = new Vector3(1,1,1);
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(prefab, Vector3.zero, Quaternion.identity, transform);
+        Vector3 pos;
+        pos.x = 28.5f;
+        pos.y = 6;
+        pos.z = 27.5f;
+        transform.localPosition = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
